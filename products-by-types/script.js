e4caf3a843bf8b059c3cb010e4cf1b97f1e04a94
Anna Shalagina <atis177@mail.ru>
2016-07-07 14:53:47 +0300

    Add products page

--- products-by-types/script.js
+++ products-by-types/script.js
@@ -1,1 +1,33 @@
-// Ваш код
+var sale = [];
+var promo = [];
+var recommended = [];
+var title = [
+    "Распродажа",
+    "Промо-акция",
+    "Рекомендуемые товары"
+];
+
+$.fancybox.showLoading();
+$.getJSON( 'products.json' )
+.done(function( data ){
+    $.each( data, function( key, val ){
+        if ( val.type === 'sale' ) {
+            sale.push(this);
+        }
+        if ( val.type === 'promo' ) {
+            promo.push(this);
+        }
+        if ( val.type === 'recommended' ) {
+            recommended.push(this);
+        }
+    });
+
+    var tmpl = _.template(document.getElementById( 'items' ).innerHTML)( sale );
+    $( '#products' ).append( tmpl );
+})
+.fail(function() {
+    alert( 'Произошла ошибка' );
+})
+.always(function(){
+    $.fancybox.hideLoading();
+});
