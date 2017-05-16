904ce80c8c521a940156e92a2bf7de40b0fe20b3
ashalagina <ashalagina@oggettoweb.com>
2017-05-16 19:45:07 +0300

    init city-selector

---
+++ city-selector/src/app.js
@@ -0,0 +1,9 @@
+const CitySelector = require('./CitySelector');
+
+new CitySelector({
+    elementId: 'citySelector',
+    regionsUrl: 'http://localhost:3000/regions',
+    localitiesUrl: 'http://localhost:3000/localities',
+    saveUrl: 'http://localhost:3000/selectedRegions'
+});
+
