3348e74896996047871d19374f7cc632a77e22a1
ashalagina <ashalagina@oggettoweb.com>
2017-05-17 10:43:09 +0300

    add module city-selector

--- city-selector/src/CitySelector/index.js
+++ city-selector/src/CitySelector/index.js
@@ -1,1 +1,33 @@
-// Your code...
+const $ = require('jquery');
+
+class CitySelector {
+    constructor(options){
+        this.el = options.el;
+        this.regionsUrl = options.regionsUrl;
+        this.localitiesUrl = options.localitiesUrl;
+        this.saveUrl = options.saveUrl;
+        this.selectRegion = $('.js-select-region');
+        this._regionTemplate = $('#region-select').html();
+
+        this.initCitySelector();
+    }
+
+    initCitySelector(){
+        this.selectRegion.removeClass('hidden');
+        this._selectRegion();
+    }
+
+    _selectRegion() {
+        this.selectRegion.on('click', () => {
+            $.ajax({
+            url: this.regionsUrl
+        }).done((res) => {;
+        var regionsTmpl = _.template(this._regionTemplate);
+
+        $('#region').html(regionsTmpl({regions: res}));
+    });
+    });
+    }
+};
+module.exports = CitySelector;
+
