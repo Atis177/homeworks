fb54453ac878e59cde581d70ffeded5a5514e3db
ashalagina <ashalagina@oggettoweb.com>
2017-05-17 18:10:00 +0300

    add select region method

--- city-selector/src/CitySelector/index.js
+++ city-selector/src/CitySelector/index.js
@@ -2,31 +2,70 @@ const $ = require('jquery');
 
 class CitySelector {
     constructor(options){
-        this.el = options.el;
-        this.regionsUrl = options.regionsUrl;
-        this.localitiesUrl = options.localitiesUrl;
-        this.saveUrl = options.saveUrl;
-        this.selectRegion = $('.js-select-region');
+        this.el = $('#' + options.el);
+        this.regionsUrl      = options.regionsUrl;
+        this.localitiesUrl   = options.localitiesUrl;
+        this.saveUrl         = options.saveUrl;
+        this.loadRegion      = $('.js-load-region')
+        this.region          = $('#region');
         this._regionTemplate = $('#region-select').html();
 
         this.initCitySelector();
     }
 
-    initCitySelector(){
-        this.selectRegion.removeClass('hidden');
-        this._selectRegion();
+    initCitySelector() {
+        this.loadRegion.removeClass('hidden');
+        this._loadRegion();
     }
 
-    _selectRegion() {
-        this.selectRegion.on('click', () => {
-            $.ajax({
-            url: this.regionsUrl
-        }).done((res) => {;
-        var regionsTmpl = _.template(this._regionTemplate);
+    _loadRegion() {
+        this.el.on('click', (event) => {
+            let $elem = $(event.target);
 
-        $('#region').html(regionsTmpl({regions: res}));
-    });
-    });
+            if($elem.hasClass('js-load-region')) {
+                $.ajax({
+                    url: this.regionsUrl
+                }).done((res) => {;
+                    var regionsTmpl = _.template(this._regionTemplate);
+
+                    this.region.html(regionsTmpl({regions: res}));
+                    this._selectRegion();
+                });
+            }
+        });
+    }
+
+    _selectRegion(){
+        this.el.on('click', (event) => {
+            let $elem       = $(event.target);
+            let $city       = $('#city');
+            let $citySelect = $('#city-select');
+
+            if($elem.hasClass('js-region-select-item')) {
+                $.ajax({
+                    url: this.localitiesUrl
+                }).done((res) => {
+                    let regionsTmpl = _.template($citySelect.html());
+                    let sortedCity = $.grep(res, function(item) {
+                        return item.id === String($elem.data('region-id'));
+                    });
+
+                    $city.html(regionsTmpl({city: sortedCity[0].list}));
+                });
+            }
+        });
+    }
+
+    _selectCity(){
+        this.el.on('click', function(){
+
+        })
+    }
+
+    _sendForm(){
+        this.el.on('click', function(){
+
+        })
     }
 };
 module.exports = CitySelector;
--- city-selector/src/app.js
+++ city-selector/src/app.js
@@ -1,7 +1,7 @@
 const CitySelector = require('./CitySelector');
 
 new CitySelector({
-    elementId: 'citySelector',
+    el: 'citySelector',
     regionsUrl: 'http://localhost:3000/regions',
     localitiesUrl: 'http://localhost:3000/localities',
     saveUrl: 'http://localhost:3000/selectedRegions'
